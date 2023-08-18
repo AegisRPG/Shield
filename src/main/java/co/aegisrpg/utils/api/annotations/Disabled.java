e6c4a9356b7f4c38d0e5ca4d9e354ff9f32916d5
Cynshiii <cynthiaemleigh@gmail.com>
2023-08-17 20:16:29 -0500

    add baseline api annotations

---
+++ src/main/java/co/aegisrpg/utils/api/annotations/Disabled.java
@@ -0,0 +1,9 @@
+package co.aegisrpg.utils.api.annotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Disabled {
+}
+
