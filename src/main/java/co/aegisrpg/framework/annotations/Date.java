c872022a3f1eddeb873f6fb85c15afbb41c4e489
Cynshiii <cynthiaemleigh@gmail.com>
2023-08-24 01:03:42 -0500

    model annotation work

---
+++ src/main/java/co/aegisrpg/framework/annotations/Date.java
@@ -0,0 +1,11 @@
+package co.aegisrpg.framework.annotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Date {
+    int m();
+    int d();
+    int y();
+}
