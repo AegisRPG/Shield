fe4f08251a2513fd04051cae41c73b29fe69f9d2
Cynshiii <cynthiaemleigh@gmail.com>
2023-09-23 23:29:29 -0500

    custom menu framework creation

---
+++ src/main/java/co/aegisrpg/features/menus/api/annotations/Title.java
@@ -0,0 +1,9 @@
+package co.aegisrpg.features.menus.api.annotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Title {
+	String value();
+}
